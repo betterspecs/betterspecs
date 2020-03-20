8443e71d24384a985a89af0201bd56c5b3587b44
Lucas Moulin <aride.moulin@gmail.com>
2020-03-19 22:51:50 -0300

    Add i18n support (#212)
    * Brought i18n translation from old branch
    
    * Added french translation
    
    * Cleaned up unused files
    
    * Added pt-BR translation

---
+++ _plugins/translate.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'i18n'
+
+module Jekyll
+  module Translate
+    I18n.load_path += Dir['_i18n/*.yml']
+
+    def translate(key)
+      I18n.locale = @context.registers[:page]['locale'].intern
+      I18n.t(key)
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Translate)
--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: About
-permalink: /about/
----
-
-Halo.
